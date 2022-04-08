e2196d80ac55c585f50401a20a202526580079b2
Tony Keiser <keisertony@gmail.com>
2022-04-08 21:13:58 +0000

    Added chat functionality with socket.io

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.use(express.static(__dirname));
-const server = app.listen(3000, () => {
+app.use(bodyParser.json());
+
+const messages = [
+  {
+    name: "Jack",
+    message: "Where to, Miss?",
+  },
+  {
+    name: "Rose",
+    message: "To the stars.",
+  },
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  messages.push(req.body);
+  io.emit("message", req.body);
+  res.sendStatus(200);
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+});
+
+const server = http.listen(3000, () => {
   console.log("Server is listening on port:", server.address().port);
 });
