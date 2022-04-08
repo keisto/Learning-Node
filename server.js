13b93a13f40587761c080fc8d2ba513c5c05a34e
Tony Keiser <keisertony@gmail.com>
2022-04-07 22:22:59 -0700

    Init

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname));
+const server = app.listen(3000, () => {
+  console.log("Server is listening on port:", server.address().port);
+});
