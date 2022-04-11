8c39d750dd224c156c2eba6016df4303072f12ed
Tony Keiser <keisertony@gmail.com>
2022-04-11 02:13:56 +0000

    Feature: added db connection to get/post messages

--- server.js
+++ server.js
@@ -3,33 +3,53 @@ const bodyParser = require("body-parser");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const mongoose = require("mongoose");
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
-const messages = [
-  {
-    name: "Jack",
-    message: "Where to, Miss?",
-  },
-  {
-    name: "Rose",
-    message: "To the stars.",
-  },
-];
+// Database Stuff
+mongoose.Promise = Promise;
 
-app.get("/messages", (req, res) => {
-  res.send(messages);
+const dbUrl =
+  "mongodb+srv://keisto:YxjOCzAYPmfnzToW@cluster0.hziww.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+const Message = mongoose.model("Message", {
+  name: String,
+  message: String,
 });
 
-app.post("/messages", (req, res) => {
-  messages.push(req.body);
-  io.emit("message", req.body);
-  res.sendStatus(200);
+app.get("/messages", (req, res) => {
+  Message.find({}, (error, messages) => res.send(messages));
+});
+
+app.post("/messages", async (req, res) => {
+  try {
+    const message = new Message(req.body);
+    const savedMessage = await message.save();
+
+    const censored = await Message.findOne({ message: "badword" });
+    if (censored) {
+      await Message.deleteOne({ _id: censored.id });
+    } else {
+      io.emit("message", req.body);
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+    return console.error(error);
+  } finally {
+    // Runs after either try or fail.
+    console.log("Message post called.");
+  }
 });
 
 io.on("connection", (socket) => {
   console.log("user connected");
+});
+
+mongoose.connect(dbUrl, (error) => {
+  console.log("mongo db connection", error);
 });
 
 const server = http.listen(3000, () => {
